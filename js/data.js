5935ae6340ce318cdf14e7983b41ce2ef58cf516
ozoddd <ozoddd>
2025-04-05 15:23:15 +0500

    add

--- js/data.js
+++ js/data.js
@@ -1,7 +1,17 @@
 export default [
   {
+    title: "Bmw-car",
+    github: "https://github.com/ozoddd/bmw-car",
+    vercel: "https://bmw-car-psi.vercel.app/",
+  },
+  {
+    title: "Shtatlar",
+    github: "https://github.com/ozoddd/shtatlar",
+    vercel: "https://shtatlar.vercel.app/",
+  },
+  {
     title: "Akademnashr",
     github: "https://github.com/ozoddd/akademnashr.uyga-vazifa",
-    vercel: "https://akademnashr-uyga-vazifa.vercel.app/"
+    vercel: "https://akademnashr-uyga-vazifa.vercel.app/",
   },
 ];
