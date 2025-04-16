d7c4b24c61446d98a9c2214757a9db0406bfef1d
ozoddd <ozoddd>
2025-04-16 12:42:36 +0500

    add

--- js/data.js
+++ js/data.js
@@ -1,5 +1,15 @@
 export default [
   {
+    title: "Tech book page",
+    github: "https://github.com/ozoddd/the-book-page",
+    vercel: "https://the-book-page.vercel.app/",
+  },
+  {
+    title: "Dsmde",
+    github: "https://github.com/ozoddd/9-dars-scss-darsi",
+    vercel: "https://9-dars-scss-darsi.vercel.app/",
+  },
+  {
     title: "AnsorMed",
     github: "https://github.com/ozoddd/10-dars-uy-ishi",
     vercel: "https://10-dars-uy-ishi-iota.vercel.app/",
